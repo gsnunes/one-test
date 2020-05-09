dac366a1978336578b93ec76c38a86887cd8a389
Guilherme Nunes <gnunes@bridge-mt.com>
2020-05-09 16:45:13 -0300

    fix: Change button

--- ds/main/stories/1-Button.stories.js
+++ ds/main/stories/1-Button.stories.js
@@ -7,7 +7,7 @@ export default {
   component: Button,
 };
 
-export const Text = () => <Button onClick={action('clicked')}>Hello Button Test</Button>;
+export const Text = () => <Button onClick={action('clicked')}>Hello Button Test Test 2</Button>;
 
 export const Emoji = () => (
   <Button onClick={action('clicked')}>
